9c22da6308f14c55e841eefb7431e13a6e917411
Niraj Shrestha <njshres7@gmail.com>
2024-05-26 09:35:35 +0545

    Add project files.

---
+++ Day1/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Student_Management.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+       
+    }
+}
---
+++ Day1/Models/StudentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Management.Models
+{
+    public class StudentDTO
+    {
+        public int id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
